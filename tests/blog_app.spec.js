8f7f926af72a5ae04d23d69c4a984eaa4044de85
quntaoz <quntaozheng@gmail.com>
2024-05-01 23:29:02 -0400

    test start at zero state

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,8 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
 
 describe ('Blog app', () => {
-  beforeEach(async ({page}) => {
-    await page.goto(' http://localhost:5173/')
+  beforeEach(async ({page, request}) => {
+    await request.post('http:localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Grace Hopper',
+        username: 'ghopper',
+        password: 'cobol'
+      }
+    })
+    await page.goto('http://localhost:5173')
   })
 
   test('front page can be opened', async ({ page }) => {
